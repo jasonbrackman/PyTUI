f068a81c91fb808fa0927d3bd7370978c648ebc9
jasonbrackman <brackman@gmail.com>
2025-04-03 22:40:28 -0700

    Update example to push the thumper down. Also provided an alignment RIGHT example.

--- example.py
+++ example.py
@@ -6,19 +6,21 @@ from elements import TableLayout, Widget, Alignment, Window
 
 def beats():
     thumpers = [
-        list("#   "),
-        list("##  "),
-        list("### "),
-        list("##  "),
-        list("#   "),
-        list("##  "),
-        list("### "),
-        list("### "),
-        list("##  "),
-        list("### "),
-        list("##  "),
-        list("### "),
-        list("##  "),
+        list("              #"),
+        list("             ##"),
+        list("            ###"),
+        list("         ######"),
+        list("             ##"),
+        list("           ####"),
+        list("         ######"),
+        list("     ##########"),
+        list("       ########"),
+        list("            ###"),
+        list("             ##"),
+        list("            ###"),
+        list("             ##"),
+        list("         ######"),
+        list("             ##"),
     ]
     while True:
         for thump in thumpers:
@@ -27,12 +29,12 @@ def beats():
 def _main():
     table = TableLayout()
     letters = Widget(list("abcdefghijklmno"), padding=2)
-    spinner = Widget(list("    "), alignment=Alignment.CENTER, padding=0)
-    table.add_coloumn(spinner, '#')
+    spinner = Widget(list("    "), alignment=Alignment.CENTER, padding=2)
+    table.add_coloumn(spinner, '##')
     table.add_coloumn(letters, "Letters")
     table.add_coloumn(Widget(["asdf", 'were', 'fizz', 'buzz', 'bang', '23432', 'vxXxv'], alignment=Alignment.CENTER, padding=8), "Words")
     table.add_coloumn(Widget(list("ABCDEFGHIJK"), padding=2), "Caps")
-    table.add_coloumn(Widget(["Super", "Cali", "Fragilistic"], padding=2), "Magic")
+    table.add_coloumn(Widget(["Super", "Cali", "Fragilistic"], alignment=Alignment.RIGHT, padding=2), "Magic")
 
     window = Window()
     window.add(Widget(list(["Example Display"]), alignment=Alignment.CENTER, padding=2))
@@ -43,7 +45,7 @@ def _main():
     for _ in range(200):
         # clear windows command screen
         os.system('cls' if os.name == 'nt' else 'clear')
-        thump = next(thumps)
+        thump = [item * 2 for item in next(thumps)]
         spinner._items = thump
         items = choices(list("abcX*defghijklmnopqrstuvwxyz"), k=1)
         letters._items.pop(0)
